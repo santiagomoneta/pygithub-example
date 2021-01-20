a60c0705b469f0075d887cbcecc94d220961caab
Santiago Moneta <smoneta@salesforce.com>
2021-01-20 15:25:39 -0300

    added string manipulation
    the file update is now partial to a particular string

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # pygithub-example
-Simple example of the usage of the PYGITHUB python module
+Simple example of the usage of the PyGithub python module
 
 
 Source:
 https://pygithub.readthedocs.io/en/latest/introduction.html
 
-TO DO:
-Add how to partially update a file, like replacing a string on a specific file. 
+The main goal of this script is to be used as a starting point for anyone who want to work with repos using python and avoid mimmicin que huma behaviour (for example using the os module and the regular git commands).
+
+This sample script will:
+
+1) Login into github using a personal access token.
+2) Get and print all the repos for the user under the token.
+3) create a new repo called "sandbox", with a sample file (test.txt) in the master branch.
+4) Create a new developer branch based on master.
+5) Update part of the content in text.txt (developer branch).
+6) Create a pull request to merge developer into master.
+
--- main.py
+++ main.py
@@ -4,46 +4,62 @@
 # The goal is to demonstrate how to login, create files and updates files in github
 
 from github import Github
+import string
+import re
 
 # Login to github
 # g = Github('username','password')
-g = Github('**********token************')
+g = Github('e9ab6bdaXXXXXXXXXXXXXXXXX5e2dc7d2db0')
+print('--> Logged in')
 
 # Get User from the token used to login
 user = g.get_user()
+print('--> Loaded username from token')
 
 # Load all user's repos for in memory
-print()
+
 repos = g.get_user(user.login).get_repos()
+print('--> Loaded user repos')
 
 # Print all the user's repo names
-print()
+
+print('--> Print all repos:')
 for repo in repos:
     print(repo.name)
 
 # create a new repo named "sandbox"
-print()
+
 repo = user.create_repo("sandbox")
+print('--> Repository "sandbox" created.')
 
 # Create a new file in the sandbox repo / master branch
 # since the repo is new, there is no branches so master will be "created" as well.
-print()
+
 repo = g.get_repo(user.login+"/sandbox")
-repo.create_file("test.txt", "commit message", "this is the file content", branch="master")
+body = '''
+Line 1: Message
+Line 2: Sample Text
+Line 3: yet another line
+'''
+repo.create_file("test.txt", "commit message", body , branch="master")
+print('--> File test.txt (master branch) created.')
 
 # create a developer branch from master
-print()
+
 sb = repo.get_branch('master')
 repo.create_git_ref(ref='refs/heads/' + 'developer', sha=sb.commit.sha)
+print('--> New branch "developer" created.')
 
-# Update the test.txt file in the developer branch with new content
-print()
-contents = repo.get_contents("test.txt", ref="developer")
-repo.update_file(contents.path, "CommitMessage", "This is new content", contents.sha, branch="developer")
+# Replace text from line 2 in test.txt from developer branch
+
+file = repo.get_contents("test.txt", ref="developer")
+filePath = file.path
+newFile = ((file.decoded_content).decode('utf-8')).replace("Sample Text","A way better and improved sample Text")
+repo.update_file(filePath, "FileUpdated", newFile, file.sha, branch="developer")
+print('--> File test.txt (developer branch) updated.')
+
 
 # Create pull request to merge the developer branch with the master branch
-print()
-
 body = '''
 SUMMARY
 Change HTTP library used to send requests
@@ -53,5 +69,5 @@ TESTS
  - [x] Send 'POST' request with/without body
 '''
 pr = repo.create_pull(title='Pull Request title', body=body, head="developer", base="master")
-print()
-print('Pull request #', pr.number, ' created.')
+
+print('--> Pull request #', pr.number, ' created.')
